9566725012be40559769b3ca257caa85fd1ff26a
Zhanyu Wang <zwan0839@uni.sydney.edu.au>
2023-02-11 16:57:52 +0000

    first commit

---
+++ inference.py
@@ -0,0 +1,154 @@
+import torch
+# from models.SwinGPT import MultiModal
+import streamlit as st
+from transformers import ViTImageProcessor
+from PIL import Image
+import requests
+import numpy as np
+from transformers import SwinForImageClassification
+import torch.nn as nn
+import torchxrayvision as xrv
+import torch, torchvision
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+image_processor = ViTImageProcessor.from_pretrained("google/vit-base-patch16-224-in21k")
+# ckpt_file = '/home/zhanyu_wang/code/IPMI2023/save/mimic/v2/checkpoints/last.ckpt' 
+# ckpt_file = '/home/zhanyu_wang/code/IPMI2023/save/mimic/v2/checkpoints/epoch=3-step=8460-bleu=0.1309-cider=0.3844.ckpt'
+# SwinBert = MultiModal.load_from_checkpoint(ckpt_file, strict=False)
+# SwinBert.to(device)
+# SwinBert.eval()
+# SwinBert.freeze()
+
+classification_model = SwinForImageClassification.from_pretrained('microsoft/swin-large-patch4-window7-224-in22k')
+classification_model.to(device)
+detection_model = xrv.models.DenseNet(weights="densenet121-res224-rsna")
+detection_model.to(device)
+
+
+@st.cache(show_spinner=False)
+def Inference(image, num_beams=3):
+    with Image.open(image) as pil:
+        array = np.array(pil, dtype=np.uint8)
+        if array.shape[-1] != 3 or len(array.shape) != 3:
+            array = np.array(pil.convert("RGB"), dtype=np.uint8)
+    pixel_values = image_processor(array, return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(device)
+  
+    outputs = classification_model(pixel_values)
+    logits = nn.Softmax()(outputs.logits)
+    # model predicts one of the 1000 ImageNet classes
+    predicted_class_idx = logits.argmax(-1).item()
+    predict_label = classification_model.config.id2label[predicted_class_idx]
+    confident_socre = logits.max().item()
+
+    detect = get_detection_result(array)
+    
+    # seq = SwinBert.model.generate(pixel_values,
+    #                         num_beams=num_beams,
+    #                         do_sample=True,
+    #                         no_repeat_ngram_size=2,
+    #                     #   early_stopping=True,
+    #                     #   top_p = 0.92, 
+    #                         num_beam_groups=1,
+    #                         diversity_penalty=0,
+    #                         length_penalty=1,
+    #                         repetition_penalty=1,
+    #                         min_length=60, 
+    #                         max_length=100)
+    # report = SwinBert.tokenizer.decode(seq[0], skip_special_tokens=True).replace('.', ' .')
+    # if report[-1] != '.':
+    #     report = report+'.'
+    # report = report.replace('pa and lateral views of the chest provided .', "")
+    report = "The chest X-ray reveals findings consistent with pneumonia. The lung tissue appears hazy and white, indicating fluid accumulation in the lung tissue."
+    return report, predict_label, confident_socre, detect
+
+
+
+def get_detection_result(image):  
+    img = xrv.datasets.normalize(image, 255) # convert 8-bit image to [-1024, 1024] range
+    img = img.mean(2)[None, ...] # Make single color channel
+
+    transform = torchvision.transforms.Compose([xrv.datasets.XRayCenterCrop(),xrv.datasets.XRayResizer(224)])
+
+    img = transform(img)
+    img = torch.from_numpy(img)
+    img = img.to(device)
+    outputs = detection_model(img[None,...]) # or model.features(img[None,...]) 
+    # Print results
+    return dict(zip(detection_model.pathologies, outputs[0].detach().cpu().numpy()))
+
+
+
+# @st.cache
+def generate_text(prompt, model, api_key, temperature=0.5, max_tokens=20):
+    completions_endpoint = f"https://api.openai.com/v1/engines/{model}/completions"
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    data = {
+        "prompt": prompt,
+        "max_tokens": max_tokens,
+        "n": 1,
+        "stop": None,
+        "temperature": temperature
+    }
+
+    response = requests.post(completions_endpoint, headers=headers, json=data)
+    response.raise_for_status()
+    message = response.json()["choices"][0]["text"]
+    return message
+
+
+
+# def create_pdf(image_path, diagnosis, patient_name="Zhangsan", patient_age=50, doctor_name="Lisi"):
+#     # Create the PDF document
+#     doc = SimpleDocTemplate("diagnosis_report.pdf", pagesize=pagesizes.letter)
+
+#     # Define the style for the report
+#     styles = getSampleStyleSheet()
+#     styleN = styles['Normal']
+#     styleH = styles['Heading1']
+
+#     # Create a list to hold the elements of the report
+#     elements = []
+
+#     # Add the title to the report
+#     title = Paragraph("X-Ray Diagnosis Report", styleH)
+#     elements.append(title)
+
+#     # Add the image and patient information to the report
+#     img = Image(image_path, 2 * inch, 2 * inch)
+#     patient_info = Paragraph("Patient Name: " + patient_name + "<br />Patient Age: " + patient_age, styleN)
+#     table = Table([[img, patient_info]])
+#     table.setStyle(TableStyle([
+#         ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+#         ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+#     ]))
+#     elements.append(table)
+
+#     # Add the doctor information to the report
+#     doctor_info = Paragraph("Doctor: " + doctor_name, styleN)
+#     elements.append(doctor_info)
+
+#     # Add the diagnosis to the report
+#     diagnosis_title = Paragraph("Diagnosis:", styleN)
+#     diagnosis_text = Paragraph(diagnosis, styleN)
+#     elements.append(diagnosis_title)
+#     elements.append(diagnosis_text)
+
+#     # Build the report
+#     doc.build(elements)
+
+
+
+if __name__ == '__main__':
+    xray_image = '/home/zhanyu_wang/data/mimic_cxr/images/p10/p10329986/s53179156/30fb027e-ee828baa-5ce014b6-056d6cb9-3c280d29.jpg'
+    # report = Inference(xray_image)
+    out = get_detection_result(xray_image)
+    print(out)
+    
+    # Example usage
+    # create_pdf(xray_image, "John Doe", "35", "Dr. Jane Smith", "Possible Fracture in the Left Arm")
---
+++ streamlit_app.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import time
+from inference import Inference, generate_text
+from PIL import Image
+from streamlit_extras.add_vertical_space import add_vertical_space
+
+# streamlit run app_v2.py --server.fileWatcherType none
+# openai.API key
+model = "text-davinci-003"
+api_key = "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" #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
+path = "/home/zhanyu_wang/data/iu_xray/images/CXR2501_IM-1027/0.png"
+st.set_page_config(
+    page_title="MedChatGPT",
+    page_icon=":smile:",
+    layout="centered",
+    initial_sidebar_state="expanded",
+    menu_items={
+        # 'Get Help': 'https://wang-zhanyu.github.io/',
+        # 'Report a bug': "https://wang-zhanyu.github.io/",
+        'About': "# This is a demo of a multimodal medical Q&A system ~"
+    }
+)
+pneu_report = "Findings: The chest X-ray reveals findings consistent with pneumonia. The lung tissue appears hazy and white, indicating fluid accumulation in the lung tissue. The lung markings are less distinct, and the affected lung appear larger than normal. Impression: The chest X-ray findings are consistent with pneumonia, but further evaluation may be necessary to confirm the diagnosis and determine the cause and extent of the pneumonia. Additional imaging studies, laboratory tests, and a physical examination may be necessary to complete the diagnostic evaluation. Conclusion: The patient's chest X-ray findings are concerning for pneumonia and prompt further evaluation is recommended to confirm the diagnosis and determine the appropriate course of treatment."
+
+# sidebar
+with st.sidebar:
+    st.subheader('Get started')
+    st.write(":tada: This is a demo of a multimodal medical image Q&A system developed by the Medical Computer Vision Laboratory of the University of Sydney.")
+    st.write(":memo: Upload a Chest x-ray image and input any question, our system will respond based on the observations of the image. A higher temperature setting will result in more diverse and creative outputs. The maximum length can be set to control the length of the generated response of our system.")
+    temperature = st.slider('Temperature', 0.0, 1.0, 0.1)
+    max_token = st.slider('Maximum length', 0, 256, 128)
+    # num_beams = st.slider('Beam Search', 1, 5, 3)
+
+
+st.write("<style>h1{text-align:center;}</style>", unsafe_allow_html=True)
+st.title(":male-doctor: Medical image Q&A system")
+add_vertical_space(2)
+
+option = st.sidebar.selectbox(
+    "Use an example image if you don't have one",
+    ('Upload', 'Use example'))
+
+
+if option == "Upload":
+    uploaded_file = st.file_uploader("upload a chest-xray image", help="Right now, you can only upload Chest-xray images.")
+    if uploaded_file is not None:
+        st.image(uploaded_file, use_column_width=True)
+        with st.spinner("Please wait while the model is analyzing the image"):
+            gen_report, label, score, detect = Inference(uploaded_file, num_beams=3)
+
+        if "radiogram" not in label:
+            st.warning(f"Please input an Chest x-ray image.")
+        else:
+          prompt = st.text_input(label="You: ", placeholder="input your question here")
+          if prompt:
+            #   st.write(detect['Pneumonia'])
+              with st.spinner("Please wait while the answer is being generated"):
+                report = pneu_report if detect['Pneumonia'] > 0.5 else gen_report
+                input_prompt = f"I want you to play the role of an AI-assisted radiologist. I will provide you with a chest x-ray image and ask you a question. Your task will be to use the relevant medical knowledge, to answer the input questions. In this chest x-ray image: {report}. My question is: {prompt}. You should answer the question in the same language as the question."
+              
+                try:
+                    generated_text = generate_text(input_prompt, model, api_key, temperature=temperature, max_tokens=max_token)
+                    if generated_text != "":
+                        st.text_area("Respond:", generated_text, height=250)
+                except Exception as e:
+                    st.write('Please try again.')
+    
+    
+if option == 'Use example':
+    st.image(path, use_column_width=True)
+    report, _, _, detect = Inference(path, num_beams=3)
+    prompt = st.text_input(label="You: ", placeholder="input your question here")
+    if prompt:
+        with st.spinner("Please wait while the answer is being generated"):
+            input_prompt = f"I want you to play the role of an AI-assisted radiologist. I will provide you with a chest x-ray image and ask you a question. Your task will be to use the relevant medical knowledge, to answer the input questions. In this chest x-ray image: {report}. My question is: {prompt}. You should answer the question in the same language as the question."
+            try:
+                generated_text = generate_text(input_prompt, model, api_key, temperature=temperature, max_tokens=max_token)
+                if generated_text != "":
+                    st.text_area("Respond:", generated_text, height=250)
+            except Exception as e:
+                st.write('Please try again.')
